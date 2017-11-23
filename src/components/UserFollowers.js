a867f88561fc9e57a8685c6bef7534480431326a
Mateusz Bielak <mt.bielak@gmail.com>
2017-11-23 19:17:12 +0100

    Fixed naming convention

--- src/components/UserFollowers.js
+++ src/components/UserFollowers.js
@@ -5,9 +5,9 @@ import UserFollowersCounter from './UserFollowersCounter';
 const UserFollowers = props => (
   <div className="user_followers">
     <div className="user_followers__stats">
-      <UserFollowersCounter user_stats={props.likes} />
-      <UserFollowersCounter user_stats={props.following} />
-      <UserFollowersCounter user_stats={props.followers} />
+      <UserFollowersCounter userStats={props.likes} />
+      <UserFollowersCounter userStats={props.following} />
+      <UserFollowersCounter userStats={props.followers} />
     </div>
     <button className="user_followers__button" onClick={props.toggleFollowers}>Follow</button>
   </div>
--- src/components/UserFollowersCounter.js
+++ src/components/UserFollowersCounter.js
@@ -4,16 +4,16 @@ import React from 'react';
 const UserFollowersCounter = props => (
   <div className="user_followers__box">
     <p className="user_followers__values">
-      { props.user_stats[1] }
+      { props.userStats[1] }
     </p>
     <p className="user_followers__names">
-      { props.user_stats[0] }
+      { props.userStats[0] }
     </p>
   </div>
 );
 
 UserFollowersCounter.propTypes = {
-  user_stats: PropTypes.array.isRequired,
+  userStats: PropTypes.array.isRequired,
 };
 
 export default UserFollowersCounter;
